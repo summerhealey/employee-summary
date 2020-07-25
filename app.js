3d2c6b463b9609cb05424fac7bc6a409f272fbf8
Summer Healey <summerleigh.healey@gmail.com>
2020-07-25 14:29:27 -0400

    Created getManagerInfo function to collection manager information. Function collects data and then pushes the data for new Manager into the manager object. buildTeam function is referenced, but not defined.

--- app.js
+++ app.js
@@ -11,5 +11,37 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 var team = [];
 
+getManagerInfo();
+
+function getManagerInfo() {
+    prompt([
+    {
+        type: "input",
+        name: "managerName",
+        message: "Please enter the manager's name: "
+    },{
+        type: "input",
+        name: "managerId",
+        message: "Enter the manager's id: "
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "Enter the manager's email: "
+    },
+    {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "Enter the manager's office number: "
+    }
+]).then(function(answers) {
+    const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
+    team.push(manager)
+    buildTeam()
+})
+}
+
+
+
 
 
